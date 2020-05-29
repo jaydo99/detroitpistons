20ea38f47eb4d9a969171e4ecd2e728d3f2f5cb6
Jayden Yiu <jaydenyiu@Jaydens-MacBook-Pro.local>
2020-05-29 15:38:17 +1200

    meet the team page added

--- src/pages/js/team.js
+++ src/pages/js/team.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import "../css/team.css";
 
+import rose from "../../components/images/rose.png";
 
 
 export default function team() {
     return (
-        <div>
-          <h1>team</h1>  
+        <div className="teamintro">
+          <div className="playername">
+          <h1>Derrick Rose</h1>  
+          <h2>Point Guard</h2>
+        </div>
+        <img src={rose} id="rose" alt="pitcture-of-derrick-rose" />
+        <h1> #25</h1>
         </div>
     )
 }
